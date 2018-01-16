92b9671f6bfdce8fd3a964cc00d3f56813ec0cdf
Ildar Nasyrov <nasyrov@it-projects.info>
2018-01-16 09:15:54 +0000

    [FIX] queue_management: bus is now working

--- queue_management/controllers/main.py
+++ queue_management/controllers/main.py
@@ -2,37 +2,20 @@
 import werkzeug.utils
 from odoo import http
 from odoo.addons.bus.controllers.main import BusController
+from odoo.addons.bus.models.bus import dispatch
 from odoo.http import request
 
 
 class QueueManagement(BusController):
     def _poll(self, dbname, channels, last, options):
-        if options.get('screen.ticket'):
-            channels = list(channels)
-            screen_channel = (
-                request.db,
-                'screen.ticket',
-                options.get('screen.ticket')
-            )
-            channels.append(screen_channel)
+        print('\n\n\n', 'in QueueManagement _poll', '\n\n\n')
+        channels = list(channels)
+        screen_channel = (
+            request.db,
+            'queue_management.head',
+        )
+        channels.append(screen_channel)
         return super(QueueManagement, self)._poll(dbname, channels, last, options)
-
-    # @http.route('/queue/services/', auth='public')
-    # def service(self, **kw):
-    #     branch_id = int(kw.get('branch_id'))
-    #     branch = request.env['queue.management.branch'].sudo().browse(branch_id)
-    #     service_ids = request.env['queue.management.service'].sudo().search([('branch_id', '=', branch.id), ('state', '=', 'opened')])
-    #     values = {
-    #         'service_ids': service_ids,
-    #     }
-    #     return http.request.render('queue_management.queue_service', values)
-
-    # @http.route('/queue/new_ticket/', auth='public')
-    # def new_ticket(self, **kw):
-    #     service_id = int(kw.get('service_id'))
-    #     service = request.env['queue.management.service'].sudo().browse(service_id)
-    #     service.new_ticket()
-    #     return werkzeug.utils.redirect('/queue/services?branch_id={}'.format(service.branch_id.id))
 
     @http.route('/queue/screen/', auth='public')
     def screen(self, **kw):
